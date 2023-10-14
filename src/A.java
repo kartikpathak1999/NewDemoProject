5865d4bff522200f4224671814a2a664dd9c75fa
kartikpathak1999 <ktpathak2871@gmail.com>
2023-10-14 17:19:51 +0530

    msg-my second commit

--- src/A.java
+++ src/A.java
@@ -1,6 +1,13 @@
 
 public class A {
 	
+	public static void main(String[] args) {
+		
+		
+		System.out.println("zukega nahi sala");
+		
+	}
+	
 	
 
 }
--- src/B.java
+++ src/B.java
@@ -1,4 +1,13 @@
 
 public class B {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("badal barsa bijuli");
+		
+		
+		
+		
+	}
 
 }
